9f66c369ca754a233f7608077408791f87c02833
julienVF <vanderflip.julien@gmail.com>
2021-08-18 10:54:44 +0200

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+// les mots alèatoire
+var randword = ["BATEAU", "ONE PIECE", "DOCTEUR",
+"", "COLONNE", "BALISE", "VALIDE", "ALIGNEMENT",
+"TABLEAU", "CODE",];
+console.log("> "+randWord.length+" mots disponibles.");
+InputEvent (TextEvent)
